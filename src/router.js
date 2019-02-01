37a59e0334d7ded024d41be51bc9cb4bfa37a3f9
lichaozhy <lichao@or-change.cn>
2019-02-01 22:16:12 +0800

    [FIXED] value validate

--- src/router.js
+++ src/router.js
@@ -1,12 +1,29 @@
 const Router = require('koa-router');
 const db = require('./db');
 const axios = require('axios');
-const urlPrefix = "https://ebank.eximbank.gov.cn/eweb/GenTokenImg.do"
 const router = module.exports = new Router();
 
-router.post('/sample', ctx => {
-	const { value, captcha } = ctx.request.body;
-	const addCaptcha = [value, Buffer.from(captcha.split(',')[1], 'base64')]
+const httpAgent = axios.create({
+	baseURL: 'https://ebank.eximbank.gov.cn/eweb'
+});
+
+router.post('/sample', async ctx => {
+	const { value, captcha, session } = ctx.request.body;
+	const addCaptcha = [value, Buffer.from(captcha.split(',')[1], 'base64')];
+
+	const { data: result } = await httpAgent.post('/CheckTokenBeforeLogin.do', `_vTokenName=${value}`, {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Cookie': `JSESSIONID=${session}`,
+			'PE-AJAX': true,
+			'PE-ENCODING': 'UTF-8'
+		}
+	});
+	
+	if (result !== 1) {
+		return ctx.status = 400;
+	}
+
 	const sql = 'INSERT TRAINING (VALUE, CAPTCHA) VALUES (?,?)';
 	db.DBConnection.query(sql, addCaptcha, err => {
 		if (err) {
@@ -14,17 +31,21 @@ router.post('/sample', ctx => {
 			return;
 		}	
 	});
+
 	ctx.body = "add success";
 })
 
-router.get('/captcha',async ctx => {
-	const { data } = await axios.get(urlPrefix, {
+router.get('/captcha', async ctx => {
+	const { data, headers } = await httpAgent.get('/GenTokenImg.do', {
 		params: {
 			random: Math.random()
 		},
 		responseType: 'arraybuffer'
 	});
-	ctx.type = 'image/jpeg';
-	ctx.body = data;
+
+	ctx.body = {
+		captcha: data.toString('base64'),
+		session: headers['set-cookie'][0].match(/=(\S*); /)[1]
+	};
 });
 
